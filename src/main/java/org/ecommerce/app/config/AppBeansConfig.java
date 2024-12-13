eaea6f97d6d737b170596507c58345f1d14ab818
Ahmed Baz <ahmedbaz1024@gmail.com>
2024-12-14 00:53:22 +0200

    configure openAPI

--- src/main/java/org/ecommerce/app/config/AppBeansConfig.java
+++ src/main/java/org/ecommerce/app/config/AppBeansConfig.java
@@ -1,6 +1,11 @@
 package org.ecommerce.app.config;
 
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -42,5 +47,40 @@ public class AppBeansConfig {
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
+
+
+    @Bean
+    public OpenAPI openAPI() {
+        Server devServer = new Server();
+        devServer.setUrl("http://localhost:8080");
+        devServer.setDescription("Server URL in the development environment");
+
+        Server prodServer = new Server();
+        prodServer.setUrl("http://localhost:8080");
+        prodServer.setDescription("Server URL in the production environment");
+
+        Contact contact = new Contact();
+        contact.setEmail("developer.baz@gmail.com");
+        contact.setName("Ahmed Baz");
+        contact.setUrl("https://github.com/ahmed-baz");
+
+        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
+
+        final String description = """
+                This is a Spring Boot application that demonstrates how to build a RESTful service with CRUD operations,
+                using the power of Spring Security with OAuth server keycloak for authentication and authorization, and use PostgreSQL for data persistence.
+                The service manages CRUD operations for orders, and products.
+                """;
+        Info info = new Info()
+                .title("Order Service API")
+                .version("1.0.0")
+                .contact(contact)
+                .description(description)
+                .termsOfService("https://github.com/ahmed-baz/order-service/blob/master/README.md")
+                .license(mitLicense);
+
+        return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
+    }
+
 
 }
--- src/main/java/org/ecommerce/app/config/SecurityConfig.java
+++ src/main/java/org/ecommerce/app/config/SecurityConfig.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -16,12 +19,32 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final JwtAuthConverter jwtAuthConverter;
+    private final String[] PUBLIC_ENDPOINTS = {
+            "/api/v1/products/filter",
+            "/actuator/**",
+            "/error/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/index.html",
+            "/webjars/**"
+    };
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+                        .anyRequest().authenticated());
 
         http
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthConverter)));
